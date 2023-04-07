c33499ab95f3bf0194a152c2aa0d91a709eff340
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-07 12:15:20 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import ironhackersImg from "./assets/ironhackers.avif"
 import './App.css';
+import Navbar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import ApartmentList from "./components/ApartmentsList";
+import ApartmentDetails from "./components/ApartmentDetails";
+import CreateApartment from "./components/CreateApartment";
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <img src={ironhackersImg} alt="ironhackers" />
+      <h1><Navbar>
+         <NavLink to="/"><HomePage/></NavLink>
+          <NavLink to="/apartments"><ApartmentList/></NavLink>
+          <NavLink to="/apartments/:id"><ApartmentDetails/></NavLink>
+          <NavLink to="/apartments/create"><CreateApartment/></NavLink>
+
+      </Navbar></h1>
+
+      <h1><HomePage/></h1>
+      <h1><ApartmentList/></h1>
+      <h1><ApartmentDetails/></h1>
+      <h1><CreateApartment/></h1>
+     
     </div>
   );
 }
