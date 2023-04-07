213dd683c85856bb5b02b47dae7fab69d9387608
ericveganoguera <eveno229@gmail.com>
2023-04-07 12:49:38 +0200

    Iteration 3 Done

--- src/components/CreateApartment.js
+++ src/components/CreateApartment.js
@@ -1,6 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function CreateApartment() {
-    return <h1>CreateApartment</h1>;
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const navigate = useNavigate()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    axios.post(`${process.env.REACT_APP_APIURL}/apartments`,{title:title,img:image})
+    .then(response => {
+        console.log("Apartment created: " ,response)
+        navigate("/apartmentlist")
+    })
+    .catch(error=>console.log(error))
   }
-  
-  export default CreateApartment;
-  
+  return (
+    <>
+      <h1>Create an apartment!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </label>
+        <label>
+            Image:
+            <input
+            type="text"
+            name="image"
+            value={image}
+            onChange={(event) => setImage(event.target.value)}
+          />
+          <button>Create</button>
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default CreateApartment;
