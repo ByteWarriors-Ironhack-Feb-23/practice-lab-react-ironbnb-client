2ef93f72bd7c6d0bcb999abfd1d7015a038383e3
Pim Blom <pim.blom@icloud.com>
2023-04-07 15:18:10 +0200

    iteration 4,5 done

--- src/components/ApartmentDetails.js
+++ src/components/ApartmentDetails.js
@@ -1,5 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+
+
 function ApartmentDetails(){
-    return <h1>  ApartmentDetail</h1>
+
+    const [apartment, setApartment] = useState(null)
+    const [apartmentId] = useParams
+
+
+    useEffect(()=>{
+        axios.get(process.env.REACT_APP_APIURL + `/apartments${apartmentId}`)
+        .then((response )=>{
+            setApartment(response.data)
+        })
+    })
+
+    const renderApartment = ()=>{
+        return(
+            <>
+            <h1> {apartment.title}</h1>
+            <img src={apartment.img} alt={apartment.title} />
+            <h2> Price: {apartment.pricePerDay}</h2>
+            </>
+        )
+    }
+
+    return (
+        <>
+
+            {apartment ? renderApartment() : <h2> loading...</h2>
+            }
+            <Link to={"/apartments"}> go back </Link>
+        </>
+    )
 }
 
 export default  ApartmentDetails
