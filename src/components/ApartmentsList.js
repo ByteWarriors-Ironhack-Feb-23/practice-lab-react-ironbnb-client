54f9683b084a9008417298e94f01c8ba910bb373
Ismael Freitas <ismael.freitas@sapo.pt>
2023-04-07 11:54:01 +0100

    Implement Iteration 2

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,5 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function ApartmentsList() {
-    return <h1>ApartmentsList</h1>;
+
+    const [apartmentsArr, setApartmentsArr] = useState(null);
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_APIURL + "/apartments")
+            .then((response) => {
+                setApartmentsArr(response.data);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    const renderApartments = () => {
+
+
+        return (
+            <>
+
+                {apartmentsArr.map((apartment) => {
+                    return (
+                        <div className="apartment">
+                            <h2>{apartment.title}</h2>
+                            <img src={apartment.img} alt={apartment.title} />
+                            <h3>Price per Day: {apartment.pricePerDay}$</h3>
+                        </div>
+                    );
+                })}
+            </>
+
+        );
+    };
+
+
+    return (
+        <>
+            <h1>Apartments</h1>
+            <div className="apartments-container">
+                {apartmentsArr ? renderApartments() : <h1>Loading...</h1> }
+            </div>
+        </>
+    );
 }
 
 export default ApartmentsList;
