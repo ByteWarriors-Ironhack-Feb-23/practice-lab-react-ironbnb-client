61bea266dbd75d6e482e05476022654031755376
Peter Sekan <sekan.pete@gmail.com>
2023-04-07 12:28:48 +0200

    it.2 done

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const ApartmentsList = (props) => {
-  return <h1>ApartmentsList</h1>;
+  const [apartmentsList, setApartmentsList] = useState(null);
+
+  const loadData = () => {
+    axios
+      .get(process.env.REACT_APP_APIURL + "apartments/")
+      .then((response) => {
+        console.log(response);
+        setApartmentsList(response.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const renderList = () => {
+    return (
+      <section>
+        {apartmentsList.map((apartment) => {
+          return (
+            <div key={apartment.id}>
+              <h3>{apartment.title}</h3>
+            </div>
+          );
+        })}
+      </section>
+    );
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+  // REACT_APP_APIURL;
+
+  return (
+    <>
+      <h1>ApartmentsList</h1>
+      {apartmentsList ? renderList() : <h2>still loading</h2>}
+    </>
+  );
 };
 
 export default ApartmentsList;
