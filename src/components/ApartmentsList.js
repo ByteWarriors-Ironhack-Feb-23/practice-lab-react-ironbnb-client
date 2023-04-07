bd2a32ab65534d0c7c37b2cb163faec1131b4678
Pim Blom <pim.blom@icloud.com>
2023-04-07 12:54:49 +0200

    iteraiton 2

--- src/components/ApartmentsList.js
+++ src/components/ApartmentsList.js
@@ -1,6 +1,51 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 function ApartmentList(){
-    return <h1> ApartmentList
-        </h1>
+
+    const [apartmentsArr, setApartmentsArr] = useState(null)
+
+    useEffect(()=>{
+        axios.get(process.env.REACT_APP_APIURL+ "/apartments")
+        .then(response=>{
+            setApartmentsArr(response.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    const renderApartments = ()  => {
+
+        return(
+            <>
+            <h1> apartments</h1>
+
+            {apartmentsArr.map((apartment)=>{
+
+                return(
+                    <div>
+                        <h2>{apartment.title}</h2>
+                        <img src={apartment.im} alt="" />
+                        <h3>Price per day: {apartment.pricePerDay}</h3>
+
+                    </div>
+                )
+
+    
+            })}
+
+            </>
+
+        )
+    }
+
+    return (
+        <>
+            {apartmentsArr ? renderApartments() : <h1> Loading </h1>}
+        </>
+
+    )
 }
 
 export default ApartmentList
